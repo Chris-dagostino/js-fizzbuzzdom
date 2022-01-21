f454d726b14edbcfd52c2df218f3e9020a3e99bc
Chris-dagostino <dagostino.christian94@gmail.com>
2022-01-21 16:33:15 +0100

    impostazione

---
+++ main.js
@@ -0,0 +1,27 @@
+
+let contElement = document.getElementById ('container')
+
+for (let i=1 ; i <= 100 ; i++ ) {
+
+    contElement.innerHTML += "<div class='box'>" + i + "</div>"
+
+    if () {
+        contElement.innerHTML += "<div class='Fizz'>" + i + "</div>"
+    }
+    else if  () {
+        contElement.innerHTML += "<div class='Buzz'>" + i + "</div>"
+    }
+    else () {
+        contElement.innerHTML += "<div class='FlizzBuzz'>" + i + "</div>"
+
+    }
+
+}
+
+
+
+//multipli di 3 stampi Fizz
+
+//multipli di 5 stampi Buzz
+
+//multipli di 3 e 5 stampi FlizzBuzz
