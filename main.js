c2070d9b8139252fa78d856f95a7f3f9efdd9917
Chris-dagostino <dagostino.christian94@gmail.com>
2022-01-21 17:46:22 +0100

    FizzBuzzDOM

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ let contElement = document.getElementById  ('container')
 for (let i=1 ; i <= 100 ; i++ ) {
     
     if ((i % 3 == 0) && (i % 5 == 0)) {
-        contElement.innerHTML += "<div class='FizzBuzz'>" + i + "</div>"
+        contElement.innerHTML += "<div class='box FizzBuzz'> FizzBuzz </div>"
     }
     else if  (i % 3 == 0) {
-        contElement.innerHTML += "<div class='Fizz'>" + i + "</div>"
+        contElement.innerHTML += "<div class='box Fizz'> Fizz </div>"
     }
     else if (i % 5 == 0) {
-        contElement.innerHTML += "<div class='Buzz'>" + i + "</div>"
+        contElement.innerHTML += "<div class='box Buzz'> Buzz </div>"
     }
     else {
-        contElement.innerHTML += "<div class='box'>" + i + "</div>"
+        contElement.innerHTML += "<div class='box none'>" + i + "</div>"
     }
 
 }
